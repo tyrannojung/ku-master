9526bfbbc9ec8f153f99c0c2cd1436de23b40dfd
tyrannojung <tyranno2020@gmail.com>
2024-02-01 02:05:47 +0900

    update

--- Experimentation/PWA/pwa-wallet/app/layout.tsx
+++ Experimentation/PWA/pwa-wallet/app/layout.tsx
@@ -9,11 +9,48 @@ const APP_DEFAULT_TITLE = "My Awesome PWA App";
 export const metadata: Metadata = {
   manifest: "/manifest.json",
   icons: {
-    other: [
+    shortcut: "/favicon.ico",
+    apple: [
       {
         url: '/icons/splashscreens/iphone5_splash.png',
         media:
-          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/icons/splashscreens/iphone6_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/icons/splashscreens/iphoneplus_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/icons/splashscreens/iphonex_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/icons/splashscreens/ipad_splash.png',
+        media:
+          '(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/icons/splashscreens/ipadpro1_splash.png',
+        media:
+          '(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)',
+        rel: 'apple-touch-startup-image',
+      },
+      {
+        url: '/icons/splashscreens/ipadpro2_splash.png',
+        media:
+          '(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)',
         rel: 'apple-touch-startup-image',
       },
     ],
